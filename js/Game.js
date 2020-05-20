d5a39109b92fb79e553a40aa32ecdcec87fc6235
James Estrada <Jamestrada@users.noreply.github.com>
2020-05-20 17:25:18 -0600

    Game class initialization

---
+++ js/Game.js
@@ -0,0 +1,31 @@
+class Game {
+    constructor(missed, phrases, activePhrase) {
+        this.missed = missed;
+        this.phrases = phrases;
+        this.activePhrase = activePhrase;
+    }
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+        
+    }
+}
