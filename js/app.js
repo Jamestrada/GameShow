bc38b8b9752a0c6bd86d697cc9e5b255f7bc4782
Jamestrada <yeims007@gmail.com>
2020-05-22 23:55:22 -0600

    Testing Phrase class

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-const game = new Game();
-const startGame = game.startGame();
+// const game = new Game();
+// const startGame = game.startGame();
 
-game.addEventListener(startGame, () => {
+// game.addEventListener(startGame, () => {
 
-});
+// });
 
+const phrase = new Phrase('Life is like a box of chocolates');
+console.log(`Phrase - phrase: ${phrase.phrase}`);
