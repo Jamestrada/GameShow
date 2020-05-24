eb501ba8c4c3fe3ee2acb01e78dddc984feb3e55
Jamestrada <yeims007@gmail.com>
2020-05-23 23:34:18 -0600

    event listener to start game

--- js/app.js
+++ js/app.js
@@ -7,3 +7,9 @@
 
 const phrase = new Phrase('Life is like a box of chocolates');
 console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+const game = new Game();
+const startButton = document.getElementById('btn__reset');
+startButton.addEventListener('click', () => {
+    game.startGame();
+});
