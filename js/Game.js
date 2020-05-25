a339f9cb75839f0e2690696639a3e023a64f5443
Jamestrada <yeims007@gmail.com>
2020-05-24 23:59:24 -0600

    Created random phrases

--- js/Game.js
+++ js/Game.js
@@ -1,8 +1,14 @@
 class Game {
-    constructor(missed, phrases, activePhrase) {
-        this.missed = missed;
-        this.phrases = phrases;
-        this.activePhrase = activePhrase;
+    constructor() {
+        this.missed = 0;
+
+        this.phrases = createPhrases();
+
+        this.activePhrase = null;
+    }
+
+    createPhrases() {
+        return ['This is life', 'I need you now', 'Love does not exist', 'There is a God', 'Cold weather'];
     }
 
     startGame() {
--- js/app.js
+++ js/app.js
@@ -5,11 +5,14 @@
 
 // });
 
-const phrase = new Phrase('Life is like a box of chocolates');
-console.log(`Phrase - phrase: ${phrase.phrase}`);
+// const phrase = new Phrase('Life is like a box of chocolates');
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
 
 const game = new Game();
-const startButton = document.getElementById('btn__reset');
-startButton.addEventListener('click', () => {
-    game.startGame();
+game.phrases.forEach((phrase, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 });
+// const startButton = document.getElementById('btn__reset');
+// startButton.addEventListener('click', () => {
+//     game.startGame();
+// });
