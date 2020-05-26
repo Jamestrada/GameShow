a7e19950c33afc7e2f2baae4c75f4966ab2c941f
Jamestrada <yeims007@gmail.com>
2020-05-25 23:56:08 -0600

    Testing phrases

--- js/Game.js
+++ js/Game.js
@@ -1,15 +1,16 @@
 class Game {
+    phrases = this.createPhrases();
     constructor() {
-        this.missed = 0;
+        // this.missed = 0;
 
-        this.phrases = createPhrases();
+        // this.phrases = createPhrases();
 
-        this.activePhrase = null;
+        // this.activePhrase = null;
     }
 
     createPhrases() {
         return ['This is life', 'I need you now', 'Love does not exist', 'There is a God', 'Cold weather'];
-    }
+    };
 
     startGame() {
 
