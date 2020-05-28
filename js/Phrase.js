b05e404af1dff7987688f0da49730130fbe42b03
Jamestrada <yeims007@gmail.com>
2020-05-27 23:57:33 -0600

    Rendering basic html for phrases that will display

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,7 +4,11 @@ class Phrase {
     }
 
     addPhraseToDisplay() {
-
+        const phraseDiv = document.createElement('div');
+        phraseDiv.id = 'phrase';
+        phraseDiv.class = 'section';
+        const phraseUl = document.createElement('ul');
+        phraseDiv.appendChild(phraseUl);
     }
 
     checkLetter() {
